12d7b57be34c6c557c875dbf36824ae94c7187de
Semaff <semarc-zhu@yandex.ru>
2022-07-24 22:49:52 +0300

    Created LeftBar component

--- components/LeftBar.tsx
+++ components/LeftBar.tsx
@@ -1,6 +1,32 @@
+import Image from "next/image";
+import styles from "../styles/LeftBar.module.scss";
+
 const LeftBar = () => {
     return (
-        <div></div>
+        <div className={styles.leftbar}>
+            <div>
+                <div className={styles.leftbar__logo}>
+                    <Image width={35} height={35} src="/favicon.png" alt="logo" />
+                </div>
+
+                <ul className={styles.leftbar__list}>
+                    <li className={styles.leftbar__item}>
+                        <a href="#a">
+                            <Image width={25} height={25} src="/file.png" alt="file" /> Files
+                        </a>
+                    </li>
+                    <li className={styles.leftbar__item}>
+                        <a href="#a">
+                            <Image width={25} height={25} src="/get-started.png" alt="get-started" /> Get Started
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <h4 className={styles.leftbar__footer}>
+                Amount of files: 10128189
+            </h4>
+        </div>
     )
 }
 
