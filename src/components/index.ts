ea71d9e82c55838b78c1ffcb73a02872b1849fde
Semaff <semarc-zhu@yandex.ru>
2022-07-28 11:08:07 +0300

    Added Spinner + Leftbar Components

---
+++ src/components/index.ts
@@ -0,0 +1,21 @@
+import File from "./UI/File";
+import FileGrid from "./UI/FileGrid";
+import Logo from "./UI/Logo";
+import Profile from "./UI/Profile";
+import Spinner from "./UI/Spinner";
+import ActionButtons from "./ActionButtons";
+import ControlButtons from "./ControlButtons";
+import LeftBar from "./LeftBar";
+import Search from "./Search";
+
+export {
+    LeftBar,
+    ActionButtons,
+    ControlButtons,
+    File,
+    FileGrid,
+    Logo,
+    Profile,
+    Search,
+    Spinner
+}
