a64c10fe0cd091dc638c5326e0b7350b7fd282f1
Semaff <semarc-zhu@yandex.ru>
2022-07-30 20:15:59 +0300

    Created Folder model

--- server/models/models.js
+++ server/models/models.js
@@ -8,20 +8,37 @@ const User = sequelize.define("user", {
     avatar: { type: DataTypes.STRING }
 });
 
+const Folder = sequelize.define("folder", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING, allowNull: false },
+    path: { type: DataTypes.STRING, allowNull: false },
+    type: { type: DataTypes.STRING, defaultValue: "folder" }
+})
+
 const File = sequelize.define("file", {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     name: { type: DataTypes.STRING, allowNull: false },
-    ext: { type: DataTypes.STRING, allowNull: false },
+    type: { type: DataTypes.STRING, allowNull: false },
     size: { type: DataTypes.INTEGER, defaultValue: 0 },
     path: { type: DataTypes.STRING, defaultValue: '' },
     cover: { type: DataTypes.STRING, defaultValue: "placeholder.png" },
     accessLink: { type: DataTypes.STRING }
 });
 
+User.hasMany(Folder);
+Folder.belongsTo(User);
+
 User.hasMany(File);
 File.belongsTo(User);
 
+Folder.hasMany(Folder, { foreignKey: { allowNull: true } });
+Folder.belongsTo(Folder);
+
+Folder.hasMany(File);
+File.belongsTo(Folder);
+
 module.exports = {
     User,
+    Folder,
     File
 }
