569f7d24ac11961ab717a85d6bf525279213e60b
Semaff <semarc-zhu@yandex.ru>
2022-08-28 14:56:28 +0300

    Added AuthForms

---
+++ client/src/components/Forms/AuthForm.tsx
@@ -0,0 +1,68 @@
+import { ChangeEvent, FormEvent, ReactNode, useState } from "react";
+import { emailPattern, passwordPattern } from "../../utils/RegExps";
+import MyInputWithLabel from "../Inputs/MyInputWithLabel";
+
+export interface FormProps {
+    children?: ReactNode | string;
+    canSubmit?: boolean;
+    titleText: string;
+    btnText: string;
+    onSubmit: (...args: any[]) => void;
+}
+
+const AuthForm = ({ children, canSubmit, titleText, btnText, onSubmit }: FormProps) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const canSubmitForm = (canSubmit ?? true) && emailPattern.test(email) && passwordPattern.test(password);
+
+    function handleSubmit(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        onSubmit(email, password);
+    }
+
+    return (
+        <form className="form" onSubmit={(e) => handleSubmit(e)}>
+            <div className="form__header">
+                <h2 className="form__title">{titleText || "Form"}</h2>
+            </div>
+
+            <div className="form__body">
+                {children}
+
+                <MyInputWithLabel
+                    view="vertical"
+                    placeholder="asd@mail.ru"
+                    name="email"
+                    type="email"
+                    pattern={emailPattern.source}
+                    title="Invalid email address"
+                    value={email}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+                >
+                    Email:
+                </MyInputWithLabel>
+
+                <MyInputWithLabel
+                    view="vertical"
+                    placeholder="******"
+                    type="password"
+                    name="password"
+                    title="Password must contain 6 characters with at least one number and one special character"
+                    pattern={passwordPattern.source}
+                    value={password}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+                >
+                    Password:
+                </MyInputWithLabel>
+            </div>
+
+            <div className="form__footer">
+                <button type="submit" className="btn --filled" disabled={!canSubmitForm}>
+                    {btnText || "Submit"}
+                </button>
+            </div>
+        </form>
+    )
+}
+
+export default AuthForm;
---
+++ client/src/components/Forms/AuthFormWithNameFields.tsx
@@ -0,0 +1,47 @@
+import { ChangeEvent, useState } from "react";
+import { onlyLettersPattern } from "../../utils/RegExps";
+import MyInputWithLabel from "../Inputs/MyInputWithLabel";
+import AuthForm, { FormProps } from "./AuthForm";
+
+const AuthFormWithNameFields = ({ children, canSubmit, titleText, btnText, onSubmit }: FormProps) => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+
+    const canSubmitForm = (canSubmit ?? true) && onlyLettersPattern.test(firstName) && onlyLettersPattern.test(lastName);
+
+    const handleSubmit = (email: string, password: string) => {
+        onSubmit(firstName, lastName, email, password);
+    }
+
+    return (
+        <AuthForm onSubmit={handleSubmit} canSubmit={canSubmitForm} btnText={btnText} titleText={titleText}>
+            {children}
+
+            <MyInputWithLabel
+                view="vertical"
+                placeholder="John"
+                name="firstname"
+                title="You must only use letters!"
+                pattern={onlyLettersPattern.source}
+                value={firstName}
+                onChange={(e: ChangeEvent<HTMLInputElement>) => setFirstName(e.target.value)}
+            >
+                First name:
+            </MyInputWithLabel>
+
+            <MyInputWithLabel
+                view="vertical"
+                placeholder="Smith"
+                name="lastname"
+                title="You must only use letters!"
+                pattern={onlyLettersPattern.source}
+                value={lastName}
+                onChange={(e: ChangeEvent<HTMLInputElement>) => setLastName(e.target.value)}
+            >
+                Last name:
+            </MyInputWithLabel>
+        </AuthForm>
+    )
+}
+
+export default AuthFormWithNameFields;
