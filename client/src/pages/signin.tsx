09685212ba86278bfe9fcae48c14502378ff2e7c
Semaff <semarc-zhu@yandex.ru>
2022-08-28 14:54:55 +0300

    Refactored Auth pages

--- client/src/pages/signin.tsx
+++ client/src/pages/signin.tsx
@@ -4,35 +4,52 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { AuthForm, Notifier } from "../components";
-import { useAuth } from "../context/authContext";
+import { useAppDispatch } from "../hooks/useAppDispatch";
+import { useTypedSelector } from "../hooks/useTypedSelector";
+import { selectAuthError, selectAuthIsLoading, selectIsLoggedIn, signin } from "../store/slices/auth/authSlice";
 
 const Signin = () => {
-    const { isLoggedIn, authError, signin } = useAuth();
     const router = useRouter();
+    const authError = useTypedSelector(selectAuthError);
+    const authIsLoading = useTypedSelector(selectAuthIsLoading);
+    const isLoggedIn = useTypedSelector(selectIsLoggedIn);
+    const dispatch = useAppDispatch();
 
-    const handleSubmit = (username: string, password: string) => {
-        if (username && password) {
-            signin(username, password);
-        }
+    const handleSignIn = (email: string, password: string) => {
+        dispatch(signin({ email, password }));
     }
 
     useEffect(() => {
-        if (isLoggedIn) {
-            router.push("/")
+        if (isLoggedIn && !authIsLoading) {
+            router.push("/");
         }
     }, [isLoggedIn]);
 
     return (
-        <MainLayout title="Sign In | DropBox" small center>
-            <AuthForm handleSubmit={handleSubmit} btnText="Sign In" />
-            <Notifier text={authError} color="red" />
-            <h5 className={styles.info}>
-                Don't have an account?
-                <Link href="/signup">
-                    <a> Sign Up</a>
-                </Link>
-            </h5>
-        </MainLayout>
+        <>
+            <Notifier
+                text={authError?.message}
+                color="red"
+            />
+
+            <MainLayout title="Sign In | CloudBox" small withPadding>
+                <AuthForm
+                    onSubmit={handleSignIn}
+                    btnText="Sign In"
+                    titleText="Authorization"
+                />
+
+                <div className={styles.auth__footer}>
+                    <h5 className={styles.auth__redirect}>
+                        Don't have an account?
+                        <span> <Link href="/signup">
+                            <a>Sign Up</a>
+                        </Link></span>
+                    </h5>
+                </div>
+            </MainLayout>
+        </>
+
     )
 }
 
--- client/src/pages/signup.tsx
+++ client/src/pages/signup.tsx
@@ -3,36 +3,52 @@ import MainLayout from "../layouts/MainLayout";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-import { AuthForm, Notifier } from "../components";
-import { useAuth } from "../context/authContext";
+import { AuthFormWithNameFields, Notifier } from "../components";
+import { useAppDispatch } from "../hooks/useAppDispatch";
+import { useTypedSelector } from "../hooks/useTypedSelector";
+import { selectAuthError, selectAuthIsLoading, selectIsLoggedIn, signup } from "../store/slices/auth/authSlice";
 
 const Signup = () => {
-    const { isLoggedIn, authError, signup } = useAuth();
     const router = useRouter();
+    const authError = useTypedSelector(selectAuthError);
+    const authIsLoading = useTypedSelector(selectAuthIsLoading);
+    const isLoggedIn = useTypedSelector(selectIsLoggedIn);
+    const dispatch = useAppDispatch();
 
-    const handleSubmit = async (username: string, password: string) => {
-        if (username && password) {
-            signup(username, password);
-        }
+    const handleSignUp = async (firstname: string, lastname: string, email: string, password: string) => {
+        dispatch(signup({ firstname, lastname, email, password }));
     }
 
     useEffect(() => {
-        if (isLoggedIn) {
-            router.push("/")
+        if (isLoggedIn && !authIsLoading) {
+            router.push("/");
         }
     }, [isLoggedIn]);
 
     return (
-        <MainLayout title="Sign Up | DropBox" small center>
-            <AuthForm handleSubmit={handleSubmit} btnText="Sign Up" />
-            <Notifier text={authError} color="red" />
-            <h5 className={styles.info}>
-                Already have an account?
-                <Link href="/signin">
-                    <a> Sign In</a>
-                </Link>
-            </h5>
-        </MainLayout>
+        <>
+            <Notifier
+                text={authError?.message}
+                color="red"
+            />
+
+            <MainLayout title="Sign Up | CloudBox" small withPadding>
+                <AuthFormWithNameFields
+                    onSubmit={handleSignUp}
+                    btnText="Sign Up"
+                    titleText="Registration"
+                />
+
+                <div className={styles.auth__footer}>
+                    <h5 className={styles.auth__redirect}>
+                        Already have an account?
+                        <span> <Link href="/signin">
+                            <a>Sign In</a>
+                        </Link></span>
+                    </h5>
+                </div>
+            </MainLayout>
+        </>
     )
 }
 
