0153b4292934cbea5885cb5cef51cbadcc798c3e
Semaff <semarc-zhu@yandex.ru>
2022-08-29 15:21:01 +0300

    Deleted Folder model

--- server/models/models.js
+++ server/models/models.js
@@ -3,42 +3,29 @@ const { DataTypes } = require("sequelize");
 
 const User = sequelize.define("user", {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    username: { type: DataTypes.STRING, unique: true, allowNull: false },
+    firstname: { type: DataTypes.STRING, allowNull: false },
+    lastname: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, unique: true, allowNull: false },
     password: { type: DataTypes.STRING, allowNull: false },
+    diskSpace: { type: DataTypes.BIGINT, defaultValue: 1024 ** 3 * 10 },
+    usedSpace: { type: DataTypes.BIGINT, defaultValue: 0 },
     avatar: { type: DataTypes.STRING }
 });
-
-const Folder = sequelize.define("folder", {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    name: { type: DataTypes.STRING, allowNull: false },
-    path: { type: DataTypes.STRING, allowNull: false },
-    type: { type: DataTypes.STRING, defaultValue: "folder" }
-})
 
 const File = sequelize.define("file", {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     name: { type: DataTypes.STRING, allowNull: false },
     type: { type: DataTypes.STRING, allowNull: false },
     size: { type: DataTypes.INTEGER, defaultValue: 0 },
-    path: { type: DataTypes.STRING, defaultValue: '' },
-    cover: { type: DataTypes.STRING, defaultValue: "placeholder.png" },
+    path: { type: DataTypes.STRING },
+    parent: { type: DataTypes.INTEGER },
     accessLink: { type: DataTypes.STRING }
 });
-
-User.hasMany(Folder);
-Folder.belongsTo(User);
 
 User.hasMany(File);
 File.belongsTo(User);
 
-Folder.hasMany(Folder, { foreignKey: { allowNull: true } });
-Folder.belongsTo(Folder);
-
-Folder.hasMany(File);
-File.belongsTo(Folder);
-
 module.exports = {
     User,
-    Folder,
     File
 }
