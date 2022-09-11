a8a250b5e489d7d0a4b9b7accbb7fb1f1041b89a
Semaff <semarc-zhu@yandex.ru>
2022-09-11 13:49:57 +0300

    Transpiled server to use Typescript instead of Javascript

--- server/routes/routes.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express');
-const router = new Router();
-const userRouter = require('./userRouter');
-const fileRouter = require('./fileRouter');
-
-router.use('/user', userRouter);
-router.use('/files', fileRouter);
-
-module.exports = router;
