993963ae3ef0c4deaf051bdb95cd3aa81fe4792f
Semaff <semarc-zhu@yandex.ru>
2022-09-11 20:59:43 +0300

    Updated file routes

---
+++ server/src/routes/fileRouter.ts
@@ -0,0 +1,22 @@
+import Router from "express";
+import fileController from '../controllers/File/FileController';
+import checkAuthMiddleware from "../middlewares/checkAuthMiddleware";
+
+const router = Router();
+
+/* File Endpoints */
+router.get("/getall/:type?", checkAuthMiddleware, fileController.getAll);
+router.get("/getone/:link", fileController.getOne); // get file by it's access link
+router.get("/search", checkAuthMiddleware, fileController.search); // search by query
+
+router.post("/createFolder", checkAuthMiddleware, fileController.createFolder);
+router.post("/uploadFile", checkAuthMiddleware, fileController.uploadFile);
+
+router.put("/share/:id", checkAuthMiddleware, fileController.share);
+router.put("/removeShare/:id", checkAuthMiddleware, fileController.removeShare);
+
+router.put('/move', checkAuthMiddleware, fileController.move); // move fileId to new parentId
+router.put('/rename/:id', checkAuthMiddleware, fileController.rename); // rename file
+router.delete('/delete/:id', checkAuthMiddleware, fileController.delete); // delete file and his children
+
+export default router;
