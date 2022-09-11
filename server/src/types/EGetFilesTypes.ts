f7a29dd2bc6533ecfca79db063345e1506671bbf
Semaff <semarc-zhu@yandex.ru>
2022-09-11 21:24:15 +0300

    Created necessary types

---
+++ server/src/types/EGetFilesTypes.ts
@@ -0,0 +1,3 @@
+export enum EGetFilesTypes {
+    SHARED = "shared"
+}
---
+++ server/src/types/JWTUser.ts
@@ -0,0 +1,6 @@
+export interface JWTUser {
+    id: number;
+    firstname: string;
+    lastname: string;
+    email: string;
+}
