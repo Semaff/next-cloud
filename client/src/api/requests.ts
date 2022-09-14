6f47ec54e6d7f7af375a385f8cb17391bfcde3af
Semaff <semarc-zhu@yandex.ru>
2022-09-14 20:22:23 +0300

    Updated Requests API

--- client/src/api/requests.ts
+++ client/src/api/requests.ts
@@ -1,21 +1,10 @@
-import axios, { AxiosRequestConfig } from "axios";
+import axios from "axios";
 
-const guestRequest = axios.create({
-    baseURL: "http://localhost:5000/"
+const request = axios.create({
+    withCredentials: true,
+    baseURL: "http://localhost:5000/",
 });
-
-const authRequest = axios.create({
-    baseURL: "http://localhost:5000/"
-});
-
-const authInterceptor = (config: AxiosRequestConfig) => {
-    config.headers!.authorization = `Bearer ${localStorage.getItem('token')}`;
-    return config;
-}
-
-authRequest.interceptors.request.use(authInterceptor);
 
 export {
-    guestRequest,
-    authRequest
+    request,
 }
