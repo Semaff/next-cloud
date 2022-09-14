dceeeab8e6bbfc07a02bc8f939bb2b94e3d50a49
Semaff <semarc-zhu@yandex.ru>
2022-09-14 20:49:21 +0300

    Updated TUser type

--- client/src/types/TUser.ts
+++ client/src/types/TUser.ts
@@ -1,5 +1,16 @@
 export type TUser = {
-    id: string;
-    username: string;
+    id: number;
+    firstname: string;
+    lastname: string;
+    email: string;
+    diskSpace: number;
+    usedSpace: number;
+    isActivated: boolean;
     avatar: string;
+    createdAt: string;
+}
+
+export interface IUserDTO {
+    email: string,
+    password: string
 }
