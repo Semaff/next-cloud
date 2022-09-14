ff907f430e5862a22b45076814225443af3d5724
Semaff <semarc-zhu@yandex.ru>
2022-09-14 20:21:00 +0300

    Refactored LeftBar

---
+++ client/src/components/Leftbar/LeftBar.tsx
@@ -0,0 +1,76 @@
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { ChangeEvent } from "react";
+import { useAppDispatch } from "../../hooks/useAppDispatch";
+import { useTypedSelector } from "../../hooks/useTypedSelector";
+import { selectUser } from "../../store/slices/auth/authSlice";
+import { uploadFile } from "../../store/slices/files/filesSlice";
+import styles from "../../styles/blocks/LeftBar.module.scss";
+import formatSize from "../../utils/formatSize";
+import MyUpload from "../Inputs/MyUpload";
+import ProgressBar from "../ProgressBar/ProgressBar";
+import LeftBarLink from "./LeftBarLink";
+
+const LeftBar = () => {
+    const dispatch = useAppDispatch();
+    const user = useTypedSelector(selectUser);
+    const router = useRouter();
+
+    const handleUploadFile = (e: ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        const files = e.target.files;
+        if (!files) {
+            return;
+        }
+
+        for (let i = 0; i < files?.length; i++) {
+            const file = files[i];
+            dispatch(uploadFile({ path: router.asPath, file: file }));
+        }
+    }
+
+    return (
+        <div className={styles.leftbar}>
+            <div className={styles.leftbar__logo}>
+                <Image
+                    style={{ cursor: "pointer" }}
+                    width={35}
+                    height={35}
+                    src="/logo.png"
+                    alt="file"
+                    onClick={() => router.push("/")}
+                />
+            </div>
+
+            <ul className={styles.leftbar__list}>
+                <MyUpload
+                    multiple
+                    name="upload"
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => handleUploadFile(e)}
+                >
+                    Upload File
+                </MyUpload>
+
+                <LeftBarLink path="/" img="/fileIcon.png">
+                    Files
+                </LeftBarLink>
+
+                <LeftBarLink path="/shared" img="/shared.png">
+                    Shared Files
+                </LeftBarLink>
+
+                <LeftBarLink path="/profile" img="/profile.png">
+                    Profile
+                </LeftBarLink>
+            </ul>
+
+            <div className={styles.leftbar__footer}>
+                <span>Free: {formatSize((user?.diskSpace || 0) - (user?.usedSpace || 1))} </span>
+                <span>of {formatSize(user?.diskSpace || 0)}</span>
+                <ProgressBar progress={Math.ceil((user?.usedSpace || 0) / (user?.diskSpace || 1) * 100)} />
+            </div>
+        </div>
+    )
+}
+
+export default LeftBar;
---
+++ client/src/components/Leftbar/LeftBarLink.tsx
@@ -0,0 +1,27 @@
+import styles from "../../styles/blocks/LeftBar.module.scss";
+import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/router";
+
+interface LeftBarLinkProps {
+    children: string;
+    path: string;
+    img: string;
+}
+
+const LeftBarLink = ({ children, path, img }: LeftBarLinkProps) => {
+    const router = useRouter();
+
+    return (
+        <li className={`${styles.leftbar__link} ${router.asPath === path ? styles.active : ""}`}>
+            <Link href={path}>
+                <a>
+                    <Image width={25} height={25} src={img} alt={children} />
+                    {children}
+                </a>
+            </Link>
+        </li>
+    )
+}
+
+export default LeftBarLink;
