34536ad22ab0b6a7feeeea3b52dc94ecbea0f165
Semaff <semarc-zhu@yandex.ru>
2022-09-14 20:19:58 +0300

    added the necessary SVGS

---
+++ client/src/components/_SVG/index.tsx
@@ -0,0 +1,15 @@
+import ArrowLeft from "./ArrowLeft";
+import DeleteSVG from "./DeleteSVG";
+import DownloadSVG from "./DownloadSVG";
+import FolderSVG from "./FolderSVG";
+import Share from "./Share";
+import UploadSVG from "./UploadSVG";
+
+export {
+    ArrowLeft,
+    DeleteSVG,
+    DownloadSVG,
+    FolderSVG,
+    Share,
+    UploadSVG
+}
