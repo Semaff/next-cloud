6b04cbc012db07cfd0e43682cc6e53b2c19ad5da
Semaff <semarc-zhu@yandex.ru>
2022-09-14 20:24:23 +0300

    Created ProgressBar component

---
+++ client/src/components/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,15 @@
+import styles from "../../styles/blocks/ProgressBar.module.scss";
+
+interface ProgressBarProps {
+    progress: number;
+}
+
+const ProgressBar = ({ progress }: ProgressBarProps) => {
+    return (
+        <div className={styles.progress}>
+            <div className={styles.progress__value} style={{ width: `${progress}%` }} />
+        </div>
+    )
+}
+
+export default ProgressBar;
