2ea1c14179b9e1eeefd717592d7c6c38d5ebdf72
Semaff <semarc-zhu@yandex.ru>
2022-09-17 16:25:58 +0300

    Refactored Home page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,8 +1,8 @@
 import styles from "../styles/pages/Home.module.scss";
 import MainLayoutWithLeftBar from "../layouts/MainLayoutWithLeftBar";
 import { useRouter } from "next/router";
-import { MouseEvent, useEffect, useState } from "react";
-import { ActionButtons, NameForm, FileGrid, Modal, MyInputWithLabel, Profile, SortButtons, FileDrag, ContextMenu } from "../components";
+import { useEffect, useState } from "react";
+import { ActionButtons, NameForm, Modal, MyInputWithLabel, Profile, SortButtons, FileMirage, ContextMenu, FileGridWithDragNDrop } from "../components";
 import { useAppDispatch } from "../hooks/useAppDispatch";
 import { useTypedSelector } from "../hooks/useTypedSelector";
 import { selectIsLoggedIn, selectUser } from "../store/slices/auth/authSlice";
@@ -10,14 +10,15 @@ import { ESorts } from "../types/ESorts";
 import { TFile } from "../types/TFile";
 import { NextPage } from "next";
 import { wrapper } from "../store/store";
-import { selectFiles, setFilesError } from "../store/slices/files/filesSlice";
-import { createFolder, fetchFiles, moveFile } from "../store/slices/files/actions";
+import { selectFiles } from "../store/slices/files/filesSlice";
+import { createFolder, fetchFiles, renameFile } from "../store/slices/files/actions";
+
+const reservedNames = ["signin", "signup", "profile", "shared"]
 
 const Home: NextPage = () => {
-    const [isContextMenuOpen, setIsContextMenuOpen] = useState(false);
-    const [anchor, setAnchor] = useState({ x: 0, y: 0 });
-    const [isModalVisible, setIsModalVisible] = useState(false);
-    const [curSort, setCurSort] = useState<ESorts>(ESorts.NAME);
+    const [isFolderModalVisible, setIsFolderModalVisible] = useState(false);
+    const [isRenameModalVisible, setIsRenameModalVisible] = useState(false);
+    const [currentSort, setCurrentSort] = useState(ESorts.NAME);
     const [selectedFiles, setSelectedFiles] = useState<TFile[]>([]);
     const dispatch = useAppDispatch();
     const router = useRouter();
@@ -25,106 +26,30 @@ const Home: NextPage = () => {
     const user = useTypedSelector(selectUser);
     const isLoggedIn = useTypedSelector(selectIsLoggedIn);
 
-    const [isMouseDown, setIsMouseDown] = useState(false);
-    const [isDragging, setIsDragging] = useState(false);
-    const [mirageAnchor, setMirageAnchor] = useState({ x: 0, y: 0 });
-
     useEffect(() => {
         if (!isLoggedIn) {
             router.push("/signin");
         }
     }, []);
 
-    const handleShowContextMenu = (e: MouseEvent<HTMLDivElement>, file: TFile) => {
-        e.preventDefault();
-        e.stopPropagation();
-        setSelectedFiles([file]);
-
-        setIsContextMenuOpen(false);
-        const newPosition = {
-            x: e.pageX,
-            y: e.pageY,
-        };
-
-        setAnchor(newPosition);
-        setIsContextMenuOpen(true);
+    const handleRenameFile = (name: string) => {
+        const file = selectedFiles[0];
+        dispatch(renameFile({ name, file }));
+        setIsRenameModalVisible(false);
     }
 
     const handleCreateFolder = (name: string) => {
-        if (name.toLowerCase() === "profile" || name.toLowerCase() === "shared") {
-            dispatch(setFilesError("You can't create folders with reserved names!"));
-        } else {
-            dispatch(createFolder({ path: router.asPath, name }));
+        setIsFolderModalVisible(false);
+        if (!reservedNames.includes(name.toLowerCase())) {
+            return dispatch(createFolder({ path: router.asPath, name }));
         }
 
-        setIsModalVisible(false);
-    }
-
-    const handleMouseDown = (e: MouseEvent<HTMLElement>, file: TFile) => {
-        if (selectedFiles.length <= 1 && !e.ctrlKey && !e.shiftKey) {
-            setSelectedFiles([file]);
-        }
-
-        setIsMouseDown(true);
-
-        const newPosition = {
-            x: e.pageX,
-            y: e.pageY,
-        };
-
-        setMirageAnchor(newPosition);
-    }
-
-    const handleMouseUp = (e: MouseEvent<HTMLElement>, file?: TFile) => {
-        setIsMouseDown(false);
-        setIsDragging(false);
-
-        if (!file || file.type !== "dir" || selectedFiles.find(fileEl => fileEl.id === file.id)) {
-            return;
-        }
-
-        if (isDragging) {
-            const parentId = file.id;
-            for (let i = 0; i < selectedFiles.length; i++) {
-                const file = selectedFiles[i];
-                dispatch(moveFile({ fileId: file.id, parentId }))
-            }
-        }
-    }
-
-    const handleMouseMove = (e: MouseEvent<HTMLElement>) => {
-        const dx = Math.abs(e.pageX - mirageAnchor.x) >= 20;
-        const dy = Math.abs(e.pageY - mirageAnchor.y) >= 20;
-
-        if ((isMouseDown && (dx || dy)) || isDragging) {
-            setIsDragging(true);
-
-            const newPosition = {
-                x: e.pageX,
-                y: e.pageY,
-            };
-
-            setMirageAnchor(newPosition);
-        }
+        return dispatch(createFolder({ path: router.asPath, name }));
     }
 
     return (
-        <div onClick={() => setIsContextMenuOpen(false)}>
-            <FileDrag
-                isVisible={isMouseDown && isDragging}
-                onMouseMove={handleMouseMove}
-                onMouseUp={handleMouseUp}
-                coords={mirageAnchor}
-            />
-
-            <ContextMenu
-                selectedFiles={selectedFiles}
-                setIsContextMenuOpen={setIsContextMenuOpen}
-                isVisible={isContextMenuOpen}
-                coords={anchor}
-            />
-
-            <Modal isVisible={isModalVisible} setIsVisible={setIsModalVisible}>
+        <>
+            <Modal isVisible={isFolderModalVisible} setIsVisible={setIsFolderModalVisible}>
                 <NameForm
                     onSubmit={handleCreateFolder}
                     btnText="Create folder"
@@ -132,44 +57,56 @@ const Home: NextPage = () => {
                 />
             </Modal>
 
+            <Modal isVisible={isRenameModalVisible} setIsVisible={setIsRenameModalVisible}
+                style={{
+                    width: "30rem",
+                    borderRadius: "3px"
+                }}>
+                <NameForm
+                    onSubmit={handleRenameFile}
+                    btnText="Rename file"
+                    labelText="Enter new file's name"
+                />
+            </Modal>
+
             <MainLayoutWithLeftBar>
                 <div className={styles.home}>
-                    <div className={styles.home__block}>
-                        <MyInputWithLabel view="horizontal" styles={{ maxWidth: "40rem" }}>
-                            Search
-                        </MyInputWithLabel>
+                    <div className={styles.home__head}>
+                        <div className={styles.home__block}>
+                            <MyInputWithLabel view="horizontal" styles={{ maxWidth: "40rem" }}>
+                                Search
+                            </MyInputWithLabel>
 
-                        <Profile user={user} />
+                            <Profile user={user} />
+                        </div>
+
+                        <h1 className={styles.home__title}>Your Files</h1>
+
+                        <div className={styles.home__block}>
+                            <ActionButtons
+                                selectedFiles={selectedFiles}
+                                setSelectedFiles={setSelectedFiles}
+                                setIsModalVisible={setIsFolderModalVisible}
+                            />
+                            <SortButtons
+                                currentSort={currentSort}
+                                setCurrentSort={setCurrentSort}
+                            />
+                        </div>
                     </div>
 
-                    <h1 className={styles.home__title}>Your Files</h1>
-
-                    <div className={styles.home__block}>
-                        <ActionButtons
+                    <div className={styles.home__body}>
+                        <FileGridWithDragNDrop
+                            currentSort={currentSort}
+                            files={files}
                             selectedFiles={selectedFiles}
                             setSelectedFiles={setSelectedFiles}
-                            setIsModalVisible={setIsModalVisible}
-                        />
-                        <SortButtons
-                            curSort={curSort}
-                            setCurSort={setCurSort}
+                            setIsRenameModalVisible={setIsRenameModalVisible}
                         />
                     </div>
-
-                    <FileGrid
-                        curSort={curSort}
-                        files={files}
-                        selectedFiles={selectedFiles}
-                        setSelectedFiles={setSelectedFiles}
-                        handleShowContextMenu={handleShowContextMenu}
-                        setIsContextMenuOpen={setIsContextMenuOpen}
-                        onMouseDown={handleMouseDown}
-                        onMouseUp={handleMouseUp}
-                        onMouseMove={handleMouseMove}
-                    />
                 </div>
             </MainLayoutWithLeftBar>
-        </div>
+        </>
     )
 }
 
