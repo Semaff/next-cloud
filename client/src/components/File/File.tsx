4c17d01638d6cc57142f5be85e6c5a53c7d6553a
Semaff <semarc-zhu@yandex.ru>
2022-09-17 16:26:57 +0300

    Refactored File component

--- client/src/components/File/File.tsx
+++ client/src/components/File/File.tsx
@@ -1,37 +1,69 @@
-import { TFile } from "../../types/TFile";
-import styles from "../../styles/blocks/File.module.scss";
+import styles from "./File.module.scss";
 import formatSize from "../../utils/formatSize";
-import Image from "next/image";
+import { TFile } from "../../types/TFile";
 import { useRouter } from "next/router";
-import { MouseEvent } from "react";
+import { MouseEvent, MutableRefObject } from "react";
 import { cutLongWord } from "../../utils/cutLongWord";
-import Share from "../_SVG/Share";
+import { Share } from "../_SVG";
+import { Anchor } from "../../types/Anchor";
+import { getElementOffset } from "../../utils/getElementProperties";
 
 export interface FileProps {
     file: TFile;
+    fileGridRef: MutableRefObject<null | HTMLElement>;
+    files: TFile[];
+
     selectedFiles: TFile[];
     setSelectedFiles: (files: TFile[]) => void;
-    handleShowContextMenu: (e: MouseEvent<HTMLDivElement>, file: TFile) => void;
-    handleFileClick: (e: MouseEvent<HTMLDivElement>, file: TFile) => void;
+
+    setAnchor: (anchor: Anchor | ((prev: Anchor) => Anchor)) => void;
+    setIsContextMenuOpen: (isContextMenuOpen: boolean | ((prev: boolean) => boolean)) => void;
+
     onMouseDown?: (e: MouseEvent<HTMLElement>, file: TFile) => void;
     onMouseUp?: (e: MouseEvent<HTMLElement>, file?: TFile) => void;
 }
 
-const File = (props: FileProps) => {
-    const {
-        file,
-        handleFileClick,
-        handleShowContextMenu,
-        onMouseDown,
-        onMouseUp,
-        selectedFiles,
-        setSelectedFiles
-    } = props;
-
+const File = ({ file, fileGridRef, files, selectedFiles, setSelectedFiles, setAnchor, setIsContextMenuOpen, onMouseDown, onMouseUp }: FileProps) => {
     const isSelected = selectedFiles.find(fileEl => fileEl.id === file.id);
+    const isImage = file.type === "image";
+    const isDir = file.type === "dir";
     const router = useRouter();
 
-    const handleOpenFolder = () => {
+    /* Mouse Events */
+    const handleClick = (e: MouseEvent<HTMLDivElement>) => {
+        e.stopPropagation();
+        setIsContextMenuOpen(false);
+        if (!e.ctrlKey && !e.shiftKey) {
+            return setSelectedFiles([file]);
+        }
+
+        if (e.shiftKey) {
+            const startPoint = files.indexOf(selectedFiles[0]) || 0;
+            const endPoint = files.indexOf(file) || 0;
+
+            const isGreater = startPoint < endPoint;
+            const newSelectedFiles: TFile[] = [];
+            if (isGreater) {
+                for (let i = startPoint; i <= endPoint; i++) {
+                    newSelectedFiles.push(files[i]);
+                }
+            } else {
+                for (let i = endPoint; i <= startPoint; i++) {
+                    newSelectedFiles.push(files[i]);
+                }
+            }
+
+            return setSelectedFiles(newSelectedFiles);
+        }
+
+        if (isSelected) {
+            setSelectedFiles([...selectedFiles.filter(fileEl => fileEl.id !== file.id)]);
+        } else {
+            setSelectedFiles([...selectedFiles, file]);
+        }
+    }
+
+    const handleDoubleClick = () => {
         if (file.type === "dir") {
             setSelectedFiles([]);
             const parsedPath = router.asPath.slice(1);
@@ -39,55 +71,59 @@ const File = (props: FileProps) => {
         }
     }
 
-    const handleClick = (e: MouseEvent<HTMLDivElement>) => {
+    const handleContextMenu = (e: MouseEvent<HTMLDivElement>, file: TFile) => {
+        e.preventDefault();
         e.stopPropagation();
-        handleFileClick(e, file);
+        setSelectedFiles([file]);
+
+        if (!fileGridRef.current) {
+            return;
+        }
+
+        const { offsetX, offsetY } = getElementOffset(fileGridRef.current);
+        setIsContextMenuOpen(false);
+        const newPosition = {
+            x: e.pageX - offsetX + 30,
+            y: e.pageY - offsetY + 45,
+        };
+
+        setAnchor(newPosition);
+        setIsContextMenuOpen(true);
     }
 
     return (
-        <>
-            <div
-                className={`${styles.file} ${isSelected ? styles.selected : ""}`}
-                onClick={handleClick}
-                onMouseDown={onMouseDown ? (e) => onMouseDown(e, file) : () => ({})}
-                onMouseUp={onMouseUp ? (e) => onMouseUp(e, file) : () => ({})}
-                onDoubleClick={handleOpenFolder}
-                onContextMenu={e => handleShowContextMenu(e, file)}
-                title={file.name}
-                unselectable="on"
-            >
-                {file.type === "image"
-                    ?
-                    <img
-                        className={styles.file__image}
-                        width={70}
-                        height={80}
-                        src={`http://localhost:5000/${file.path}`}
-                        alt={file.name}
-                    />
-                    :
-                    <Image
-                        className={styles.file__image}
-                        width={70}
-                        height={80}
-                        src={file.type === "dir" ? "/folder.png" : "/file.png"}
-                        alt={file.name}
-                    />
-                }
+        <div
+            onClick={handleClick}
+            onDoubleClick={handleDoubleClick}
+            onContextMenu={e => handleContextMenu(e, file)}
 
-                {file.accessLink
-                    ? (
-                        <div className={styles.file__share}>
-                            <Share />
-                        </div>
-                    )
-                    : null
-                }
+            onMouseDown={onMouseDown ? (e) => onMouseDown(e, file) : () => ({})}
+            onMouseUp={onMouseUp ? (e) => onMouseUp(e, file) : () => ({})}
 
-                <span className={styles.file__name}>{cutLongWord(file.name)}</span>
-                <span className={styles.file__size}>{formatSize(file.size)}</span>
-            </div>
-        </>
+            className={`${styles.file} ${isSelected ? styles.selected : ""}`}
+            title={file.name}
+            unselectable="on"
+        >
+            <img
+                className={styles.file__image}
+                width={70}
+                height={80}
+                src={isImage
+                    ? `${process.env.NEXT_PUBLIC_SERVER_URL}/${file.path}`
+                    : isDir ? "/folder.png" : "/file.png"
+                }
+                alt="file"
+            />
+
+            {file.accessLink && (
+                <div className={styles.file__share}>
+                    <Share />
+                </div>
+            )}
+
+            <span className={styles.file__name}>{cutLongWord(file.name)}</span>
+            <span className={styles.file__size}>{formatSize(file.size)}</span>
+        </div>
     )
 }
 
