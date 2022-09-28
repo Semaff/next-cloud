93359f98620945bb37a4c682a70727704cd270ec
Semaff <semarc-zhu@yandex.ru>
2022-09-28 12:24:05 +0300

    Removed GoBack button

--- client/src/components/Buttons/ActionButtons.tsx
+++ client/src/components/Buttons/ActionButtons.tsx
@@ -8,81 +8,66 @@ import { request } from "../../api/requests";
 import { deleteFile } from "../../store/slices/files/actions";
 
 interface ActionButtonsProps {
-    setSelectedFiles: (files: TFile[]) => void;
-    setIsModalVisible: (isVisible: boolean) => void;
-    selectedFiles: TFile[];
+  setSelectedFiles: (files: TFile[]) => void;
+  setIsModalVisible: (isVisible: boolean) => void;
+  selectedFiles: TFile[];
 }
 
 const ActionButtons = memo(({ selectedFiles, setSelectedFiles, setIsModalVisible }: ActionButtonsProps) => {
-    const dispatch = useAppDispatch();
-    const router = useRouter();
+  const dispatch = useAppDispatch();
+  const router = useRouter();
 
-    const goBackToOneFolder = () => {
-        const arrayPath = router.asPath.slice(1).split("/");
-        const curPath = arrayPath.slice(0, arrayPath.length - 1);
-        router.push(`/${curPath.join("/")}`);
-        setSelectedFiles([]);
+  const downloadSelectedFiles = async () => {
+    for (let i = 0; i < selectedFiles.length; i++) {
+      const file = selectedFiles[i];
+      const response = await request.get("api/files/downloadFile/" + file.id);
+      if (response.status === 200) {
+        const blob = new Blob([await response.data]);
+        const downloadUrl = window.URL.createObjectURL(blob);
+        const link = document.createElement("a");
+        link.href = downloadUrl;
+        link.download = file.name;
+        document.body.appendChild(link);
+        link.click();
+        link.remove();
+      }
     }
 
-    const downloadSelectedFiles = async () => {
-        for (let i = 0; i < selectedFiles.length; i++) {
-            const file = selectedFiles[i];
-            const response = await request.get("api/files/downloadFile/" + file.id);
-            if (response.status === 200) {
-                const blob = new Blob([await response.data]);
-                const downloadUrl = window.URL.createObjectURL(blob);
-                const link = document.createElement("a");
-                link.href = downloadUrl;
-                link.download = file.name;
-                document.body.appendChild(link);
-                link.click();
-                link.remove();
-            }
-        }
+    setSelectedFiles([]);
+  }
 
-        setSelectedFiles([]);
+  const deleteSelectedFiles = () => {
+    for (let i = 0; i < selectedFiles.length; i++) {
+      const file = selectedFiles[i];
+      dispatch(deleteFile({ file }));
     }
 
-    const deleteSelectedFiles = () => {
-        for (let i = 0; i < selectedFiles.length; i++) {
-            const file = selectedFiles[i];
-            dispatch(deleteFile({ file }));
-        }
+    setSelectedFiles([]);
+  }
 
-        setSelectedFiles([]);
-    }
+  return (
+    <div className="btn-wrapper">
+      <MyButton onClick={() => setIsModalVisible(true)} className="--nofill --diagonal">
+        <FolderSVG /> New Folder
+      </MyButton>
 
-    return (
-        <div className="btn-wrapper">
-            <MyButton
-                onClick={goBackToOneFolder}
-                className="--nofill --diagonal"
-                disabled={router.asPath === "/"}
-            >
-                <ArrowLeft /> Go Back
-            </MyButton>
+      <MyButton
+        onClick={downloadSelectedFiles}
+        className="--nofill --diagonal"
+        disabled={selectedFiles.length === 0}
+      >
+        <DownloadSVG /> Download
+      </MyButton>
 
-            <MyButton onClick={() => setIsModalVisible(true)} className="--nofill --diagonal">
-                <FolderSVG /> New Folder
-            </MyButton>
-
-            <MyButton
-                onClick={downloadSelectedFiles}
-                className="--nofill --diagonal"
-                disabled={selectedFiles.length === 0}
-            >
-                <DownloadSVG /> Download
-            </MyButton>
-
-            <MyButton
-                onClick={deleteSelectedFiles}
-                className="--nofill --diagonal"
-                disabled={selectedFiles.length === 0}
-            >
-                <DeleteSVG /> Delete
-            </MyButton>
-        </div>
-    )
+      <MyButton
+        onClick={deleteSelectedFiles}
+        className="--nofill --diagonal"
+        disabled={selectedFiles.length === 0}
+      >
+        <DeleteSVG /> Delete
+      </MyButton>
+    </div>
+  )
 });
 
 export default ActionButtons;
