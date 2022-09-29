a592d1bef0b8d2dee91f8605e60e73491bb6493c
Semaff <semarc-zhu@yandex.ru>
2022-09-29 12:57:40 +0300

    Removed unused interface props

--- client/src/components/Modal/Uploader/Uploader.tsx
+++ client/src/components/Modal/Uploader/Uploader.tsx
@@ -7,43 +7,39 @@ import { useAppDispatch } from "../../../hooks/useAppDispatch";
 import MyButton from "../../Buttons/MyButton";
 import { useEffect } from "react";
 
-interface UploaderProps {
-    isVisible: boolean
-}
-
 const Uploader = () => {
-    const isUploaderVisible = useSelector(selectUploaderIsVisible);
-    const uploadFiles = useSelector(selectUploadFiles);
-    const dispatch = useAppDispatch();
+  const isUploaderVisible = useSelector(selectUploaderIsVisible);
+  const uploadFiles = useSelector(selectUploadFiles);
+  const dispatch = useAppDispatch();
 
-    useEffect(() => {
-        if (uploadFiles.length === 0 && isUploaderVisible !== false) {
-            dispatch(setUploaderIsVisible(false));
-        }
-    }, [uploadFiles])
+  useEffect(() => {
+    if (uploadFiles.length === 0 && isUploaderVisible !== false) {
+      dispatch(setUploaderIsVisible(false));
+    }
+  }, [uploadFiles])
 
-    return (
-        <MiniModal
-            isVisible={isUploaderVisible}
-            style={{
-                bottom: 0,
-                right: "5%",
-                maxWidth: "35rem",
-            }}>
-            <div className={styles.uploader}>
-                <div className={styles.uploader__header}>
-                    <h2 className={styles.uploader__title}>Downloads</h2>
-                    <MyButton className="--filled" onClick={() => dispatch(setUploaderIsVisible(false))}>
-                        X
-                    </MyButton>
-                </div>
+  return (
+    <MiniModal
+      isVisible={isUploaderVisible}
+      style={{
+        bottom: 0,
+        right: "5%",
+        maxWidth: "35rem",
+      }}>
+      <div className={styles.uploader}>
+        <div className={styles.uploader__header}>
+          <h2 className={styles.uploader__title}>Downloads</h2>
+          <MyButton className="--filled" onClick={() => dispatch(setUploaderIsVisible(false))}>
+            X
+          </MyButton>
+        </div>
 
-                {uploadFiles.map(file =>
-                    <UploaderFile key={file.id} file={file} />
-                )}
-            </div>
-        </MiniModal>
-    )
+        {uploadFiles.map(file =>
+          <UploaderFile key={file.id} file={file} />
+        )}
+      </div>
+    </MiniModal>
+  )
 }
 
 export default Uploader;
