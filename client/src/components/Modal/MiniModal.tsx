9dd97035969f68fd16524e3eea2fc00722ae887d
Semaff <semarc-zhu@yandex.ru>
2022-09-29 12:57:49 +0300

    Added testid prop

--- client/src/components/Modal/MiniModal.tsx
+++ client/src/components/Modal/MiniModal.tsx
@@ -2,47 +2,50 @@ import styles from "./Modal.module.scss";
 import { CSSProperties, MouseEvent, ReactNode } from "react";
 
 export interface MiniModalProps {
-    isVisible: boolean;
-    children?: ReactNode | string;
-    style?: CSSProperties;
-    isTransparent?: boolean;
-    onMouseUp?: (e: MouseEvent<HTMLDivElement>) => void;
-    onMouseDown?: (e: MouseEvent<HTMLDivElement>) => void;
-    onMouseMove?: (e: MouseEvent<HTMLDivElement>) => void;
+  isVisible: boolean;
+  children?: ReactNode | string;
+  style?: CSSProperties;
+  isTransparent?: boolean;
+  onMouseUp?: (e: MouseEvent<HTMLDivElement>) => void;
+  onMouseDown?: (e: MouseEvent<HTMLDivElement>) => void;
+  onMouseMove?: (e: MouseEvent<HTMLDivElement>) => void;
+  testid?: string;
 }
 
 const MiniModal = (props: MiniModalProps) => {
-    const {
-        isVisible,
-        children,
-        style,
-        isTransparent,
-        onMouseUp,
-        onMouseDown,
-        onMouseMove
-    } = props;
+  const {
+    isVisible,
+    children,
+    style,
+    isTransparent,
+    onMouseUp,
+    onMouseDown,
+    onMouseMove,
+    testid
+  } = props;
 
-    const miniModalStyles = [styles.minimodal];
-    if (isVisible) {
-        miniModalStyles.push(styles.visible);
-    }
+  const miniModalStyles = [styles.minimodal];
+  if (isVisible) {
+    miniModalStyles.push(styles.visible);
+  }
 
-    if (isTransparent) {
-        miniModalStyles.push(styles.transparent);
-    }
+  if (isTransparent) {
+    miniModalStyles.push(styles.transparent);
+  }
 
-    return (
-        <div
-            className={miniModalStyles.join(" ")}
-            style={style}
-            onClick={e => e.stopPropagation()}
-            onMouseUp={onMouseUp}
-            onMouseDown={onMouseDown}
-            onMouseMove={onMouseMove}
-        >
-            {children}
-        </div>
-    )
+  return (
+    <div
+      data-testid={testid}
+      className={miniModalStyles.join(" ")}
+      style={style}
+      onClick={e => e.stopPropagation()}
+      onMouseUp={onMouseUp}
+      onMouseDown={onMouseDown}
+      onMouseMove={onMouseMove}
+    >
+      {children}
+    </div>
+  )
 }
 
 export default MiniModal;
